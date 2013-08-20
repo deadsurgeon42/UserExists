7e9461a56003551829b1b873b6a2e19d7d75a03f
Simon311 <mkontent@gmail.com>
2013-08-20 18:47:49 +0400

    Added new features :)

--- UserExists/UserExists.cs
+++ UserExists/UserExists.cs
@@ -36,6 +36,7 @@ namespace UserExists
         public override void Initialize()
         {
             Commands.ChatCommands.Add(new Command("userexists", UE, "userexists", "ue"));
+            Commands.ChatCommands.Add(new Command("checkbanned", IfBanned, "banned"));
         }
 
         private void UE(CommandArgs args)
@@ -45,9 +46,11 @@ namespace UserExists
                 string uname = String.Join(" ", args.Parameters);
                 if (uname != null & uname != "")
                 {
-                    if (TShock.Users.GetUserByName(uname) != null)
+                    var user = TShock.Users.GetUserByName(uname);
+                    if (user != null)
                     {
                         args.Player.SendMessage(string.Format("User {0} exists.", uname), Color.DeepPink);
+                        if (args.Player.Group.HasPermission("viewgroup")) { args.Player.SendMessage(string.Format("{0}'s group is {1}.", uname, user.Group), Color.DeepPink); }
                     }
                     else
                     {
@@ -55,6 +58,26 @@ namespace UserExists
                     }
                 }
                 else { args.Player.SendMessage("Usage: /userexists name", Color.Red); }
+            }
+        }
+        private void IfBanned(CommandArgs args)
+        {
+            if (args.Player != null)
+            {
+                string uname = String.Join(" ", args.Parameters);
+                if (uname != null & uname != "")
+                {
+                    var ban = TShock.Bans.GetBanByName(uname);
+                    if (ban != null)
+                    {
+                        args.Player.SendMessage(string.Format("{0} (IP: {1}) is banned for: {2}", uname, ban.IP, ban.Reason), Color.DeepPink);
+                    }
+                    else
+                    {
+                        args.Player.SendMessage(string.Format("{0} is not banned.", uname), Color.DeepPink);
+                    }
+                }
+                else { args.Player.SendMessage("Usage: /banned name", Color.Red); }
             }
         }
     }
