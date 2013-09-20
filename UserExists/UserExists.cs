99379eb031b02908a609aeb62e0d7cde3133839e
Simon311 <mkontent@gmail.com>
2013-09-20 20:57:24 +0400

    Version 1.0.0.1

--- UserExists/UserExists.cs
+++ UserExists/UserExists.cs
@@ -10,7 +10,7 @@ namespace UserExists
     {
         public override Version Version
         {
-            get { return new Version("1.0.0.0"); }
+            get { return new Version("1.0.0.1"); }
         }
 		
         public override string Name
