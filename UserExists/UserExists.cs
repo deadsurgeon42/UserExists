3aaa5f14a2725871a51a642fad1600332f8112a9
Simon311 <mkontent@gmail.com>
2013-11-08 22:35:25 +0400

    1.1.0.1

--- UserExists/UserExists.cs
+++ UserExists/UserExists.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
 using TShockAPI;
 using Terraria;
 using TerrariaApi.Server;
@@ -11,7 +14,7 @@ namespace UserExists
     {
         public override Version Version
         {
-            get { return new Version("1.1.0.0"); }
+			get { return Assembly.GetExecutingAssembly().GetName().Version; }
         }
 		
         public override string Name
@@ -39,24 +42,18 @@ namespace UserExists
         {
             Commands.ChatCommands.Add(new Command("userexists", UE, "userexists", "ue"));
             Commands.ChatCommands.Add(new Command("checkbanned", IfBanned, "banned"));
-            if (!TShock.Config.AllowRegisterAnyUsername) { ServerApi.Hooks.ServerChat.Register(this, OnChat, 2); }
+            if (!TShock.Config.AllowRegisterAnyUsername) ServerApi.Hooks.ServerChat.Register(this, OnChat, 2);
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                if (!TShock.Config.AllowRegisterAnyUsername) { ServerApi.Hooks.ServerChat.Deregister(this, OnChat); }
-            }
+            if (disposing && !TShock.Config.AllowRegisterAnyUsername) ServerApi.Hooks.ServerChat.Deregister(this, OnChat);
             base.Dispose(disposing);
         }
 
         private void OnChat(ServerChatEventArgs args)
         {
-            if (args.Handled)
-            {
-                return;
-            }
+            if (args.Handled) return;
 
             TSPlayer player = TShock.Players[args.Who];
 
@@ -73,10 +70,9 @@ namespace UserExists
                 {
                     args.Handled = true;
                     player.SendMessage("We're sorry, but this name is already taken.", Color.DeepPink);
-                    if (player.Group.HasPermission("selfname")) { player.SendMessage("Please use /selfname [newname] to change your name.", Color.DeepPink); }
+                    if (player.Group.HasPermission("selfname")) player.SendMessage("Please use /selfname [newname] to change your name.", Color.DeepPink);
                 }
             }
-            return;
         }
 
 
@@ -91,16 +87,23 @@ namespace UserExists
                     if (user != null)
                     {
                         args.Player.SendMessage(string.Format("User {0} exists.", uname), Color.DeepPink);
-                        if (args.Player.Group.HasPermission("viewgroup")) { args.Player.SendMessage(string.Format("{0}'s group is {1}.", uname, user.Group), Color.DeepPink); }
+						if (args.Player.Group.HasPermission("viewgroup"))
+						{
+							List<string> KnownIps = JsonConvert.DeserializeObject<List<string>>(user.KnownIps);
+                            string ip = KnownIps[KnownIps.Count - 1];
+							args.Player.SendMessage(string.Format("{0}'s group is {1}.", uname, user.Group), Color.DeepPink);
+							args.Player.SendMessage(string.Format("{0}'s last known IP is {1}.", uname, ip), Color.DeepPink);
+						}
                     }
                     else
                     {
                         args.Player.SendMessage(string.Format("User {0} does not exist.", uname), Color.DeepPink);
                     }
                 }
-                else { args.Player.SendMessage("Usage: /userexists name", Color.Red); }
+                else args.Player.SendErrorMessage("Syntax: /userexists [name].");
             }
         }
+
         private void IfBanned(CommandArgs args)
         {
             if (args.Player != null)
@@ -113,15 +116,22 @@ namespace UserExists
                     {
                         args.Player.SendMessage(string.Format("{0} (IP: {1}) is banned for: \"{2}\"", uname, ban.IP, ban.Reason), Color.DeepPink);
                         var add = ".";
-                        if (!String.IsNullOrEmpty(ban.Expiration)) { add = " with expiration date: " + DateTime.Parse(ban.Expiration).ToUniversalTime(); }
+						bool perma = String.IsNullOrEmpty(ban.Expiration);
+						if (!perma) add = " with expiration date: " + DateTime.Parse(ban.Expiration).ToUniversalTime();
                         args.Player.SendMessage(string.Format("on {0} by user \"{1}\"{2}", DateTime.Parse(ban.Date).ToUniversalTime(), ban.BanningUser, add), Color.DeepPink);
+						if (!perma)
+						{
+							TimeSpan Remain = DateTime.Parse(ban.Expiration).ToUniversalTime() - DateTime.Now.ToUniversalTime();
+							if (Remain.TotalSeconds < 0) args.Player.SendMessage("The ban has already expired!", Color.DeepPink);
+							else args.Player.SendMessage(string.Format("The ban will expire in {0}.", Remain.ToString()), Color.DeepPink);
+						}
                     }
                     else
                     {
                         args.Player.SendMessage(string.Format("{0} is not banned.", uname), Color.DeepPink);
                     }
                 }
-                else { args.Player.SendMessage("Usage: /banned name", Color.Red); }
+                else args.Player.SendMessage("Usage: /banned name", Color.Red);
             }
         }
     }
